288050bb406af2dd22348809ac718fbb67c7fb03
vtxf <vtxf@qq.com>
2019-08-04 20:06:40 +0800

    init

---
+++ Apps/Demos/Viewer/scripts/sceneTreeCreationExample.js
@@ -0,0 +1,436 @@
+// 场景树的创建有两种方式：
+// 1 api直接调用方式
+
+function sceneTreeCreateExample1(xbsjEarth) {
+    const xbsjSceneTree = xbsjEarth.sceneTree;
+    { // 测试代码，以后删除
+        const g0 = new XE.SceneTree.Group(xbsjEarth);
+        g0.title = '项目1'
+        const g1 = new XE.SceneTree.Group(xbsjEarth);
+        g1.title = '项目2'
+        const g00 = new XE.SceneTree.Group(xbsjEarth);
+        g00.title = '当前场景'
+
+        xbsjSceneTree.root.children.push(g0);
+        xbsjSceneTree.root.children.push(g1);
+        g0.children.push(g00);
+
+        // 三维瓦片加载
+        {
+            const tileset1 = new XE.Obj.Tileset(xbsjEarth);
+            tileset1.name = '三维瓦片1x';
+            window.tileset1 = tileset1; 
+            tileset1.url = 'http://lab2.cesiumlab.com:9000/model/f15b9e90ac2d11e99dbd8fd044883638/tileset.json';
+            const tileset1SO = new XE.SceneTree.Leaf(tileset1);
+            window.tileset1SO = tileset1SO;
+            // tileset1SO.title = '三维瓦片1';
+            g00.children.push(tileset1SO);
+        }
+
+        // 三维瓦片2加载
+        {
+            const tileset2 = new XE.Obj.Tileset(xbsjEarth);
+            window.tileset2 = tileset2; 
+            tileset2.url = 'http://lab2.cesiumlab.com:9000/model/d16c1ce0ac2d11e99dbd8fd044883638/tileset.json';
+            const tileset2SO = new XE.SceneTree.Leaf(tileset2);
+            tileset2SO.title = '三维瓦片2';
+            g00.children.push(tileset2SO);
+        }
+
+        // 谷歌影像加载
+        {
+            var googleImageryLayer = new XE.Obj.Imagery(xbsjEarth);
+            window.googleImageryLayer = googleImageryLayer;
+
+            googleImageryLayer.xbsjImageryProvider.type = 'XbsjImageryProvider';
+            googleImageryLayer.xbsjImageryProvider['XbsjImageryProvider'] = {
+                url: 'http://mt1.google.cn/vt/lyrs=s&hl=zh-CN&x={x}&y={y}&z={z}&s=Gali',
+                rectangle: [-Math.PI, -Math.PI * 0.5, Math.PI, Math.PI * 0.5],
+            };;
+
+            const googleImageryLayerSO = new XE.SceneTree.Leaf(googleImageryLayer);
+            googleImageryLayerSO.title = '谷歌影像';
+            g00.children.push(googleImageryLayerSO);
+        }
+
+        // WMTS影像加载
+        {
+            var wmtsImageryLayer = new XE.Obj.Imagery(xbsjEarth);
+            window.wmtsImageryLayer = wmtsImageryLayer;
+
+            wmtsImageryLayer.xbsjImageryProvider.type = 'WebMapTileServiceImageryProvider';
+            wmtsImageryLayer.xbsjImageryProvider['WebMapTileServiceImageryProvider'] = {
+                url: 'http://basemap.nationalmap.gov/arcgis/rest/services/USGSShadedReliefOnly/MapServer/WMTS',
+                layer: 'USGSShadedReliefOnly',
+                style: 'default',
+                format: 'image/jpeg',
+                //tileMatrixSetID : 'default028mm',
+                tileMatrixSetID: 'GoogleMapsCompatible',
+                maximumLevel: 19,
+                credit: 'U. S. Geological Survey'
+            };
+ 
+
+            const imageryLayerSO = new XE.SceneTree.Leaf(wmtsImageryLayer);
+            imageryLayerSO.title = 'WMTS影像';
+            imageryLayerSO.enabled = false;
+            g00.children.push(imageryLayerSO);
+        }
+
+        // // 谷歌影像加载
+        // for (let i=0; i<4; ++i) {
+        //     var googleImageryLayer = new XE.Obj.Imagery(xbsjEarth);
+        //     window.googleImageryLayer = googleImageryLayer;
+
+        //     const imageryProvider = new XE.ImageryProvider.XbsjImageryProvider();
+        //     imageryProvider.url = 'http://www.google.cn/maps/vt?lyrs=s,h&gl=CN&x={x}&y={y}&z={z}';
+        //     imageryProvider.rectangle = [-Math.PI, -Math.PI * 0.5, Math.PI, Math.PI * 0.5];
+        //     imageryProvider.srcCoordType = 'GCJ02';
+        //     imageryProvider.dstCoordType = 'WGS84';
+        //     // googleImageryLayer.xbsjImageryProvider = imageryProvider;
+        //     googleImageryLayer.xbsjImageryProvider.type = imageryProvider._type;
+        //     googleImageryLayer.xbsjImageryProvider[imageryProvider._type] = imageryProvider;
+
+        //     const googleImageryLayerSO = new XE.SceneTree.Leaf(googleImageryLayer);
+        //     googleImageryLayerSO.title = '谷歌影像+标注';
+        //     g00.children.push(googleImageryLayerSO);
+        // }
+
+        // // 谷歌影像加载
+        // for (let i=0; i<5; ++i) {
+        //     var googleImageryLayer = new XE.Obj.Imagery(xbsjEarth);
+        //     window.googleImageryLayer = googleImageryLayer;
+
+        //     const imageryProvider = new XE.ImageryProvider.UrlTemplateImageryProvider();
+        //     imageryProvider.url = 'http://www.google.cn/maps/vt?lyrs=s,h&gl=CN&x={x}&y={y}&z={z}';
+        //     imageryProvider.rectangle = [-Math.PI, -Math.PI * 0.5, Math.PI, Math.PI * 0.5];
+        //     // googleImageryLayer.xbsjImageryProvider = imageryProvider;
+        //     googleImageryLayer.xbsjImageryProvider.type = imageryProvider._type;
+        //     googleImageryLayer.xbsjImageryProvider[imageryProvider._type] = imageryProvider;
+
+        //     const googleImageryLayerSO = new XE.SceneTree.Leaf(googleImageryLayer);
+        //     googleImageryLayerSO.title = '谷歌影像+标注';
+        //     g00.children.push(googleImageryLayerSO);
+        // }
+
+        // 百度影像加载
+        // {
+        //     var imageryLayer = new XE.Obj.Imagery(xbsjEarth);
+        //     window.baiduImageryLayer = imageryLayer;
+
+        //     const imageryProvider = new XE.ImageryProvider.XbsjImageryProvider();
+        //     imageryProvider.url = 'http://online2.map.bdimg.com/onlinelabel/?qt=tile&styles=pl&x={x}&y={y}&z={z}';
+        //     imageryProvider.rectangle = [-Math.PI, -Math.PI * 0.5, Math.PI, Math.PI * 0.5];
+        //     imageryProvider.srcCoordType = 'BD09';
+        //     // googleImageryLayer.xbsjImageryProvider = imageryProvider;
+        //     imageryLayer.xbsjImageryProvider.type = imageryProvider._type;
+        //     imageryLayer.xbsjImageryProvider[imageryProvider._type] = imageryProvider;
+        //     imageryLayer.show = false;
+
+        //     const baiduImageryLayerSO = new XE.SceneTree.Leaf(imageryLayer);
+        //     baiduImageryLayerSO.title = '百度影像';
+        //     g00.children.push(baiduImageryLayerSO);
+        // }
+
+        // 中国地形加载
+        {
+            var chinaTerrain = new XE.Obj.Terrain(xbsjEarth);
+            chinaTerrain.show = false;
+            chinaTerrain.xbsjTerrainProvider = {
+                XbsjCesiumTerrainProvider: { 
+                   url:'http://lab2.cesiumlab.com:9000/terrain/577fd5b0ac1f11e99dbd8fd044883638'
+                },
+                type: 'XbsjCesiumTerrainProvider'
+            }
+
+            window.chinaTerrain = chinaTerrain;
+
+            const chinaTerrainSO = new XE.SceneTree.Leaf(chinaTerrain);
+            chinaTerrainSO.title = '中国地形';
+            g00.children.push(chinaTerrainSO);
+        }
+
+     
+        // 视频融合
+        {
+            var cameraVideo1 = new XE.Obj.CameraVideo(xbsjEarth);
+            cameraVideo1.videoUrl = '../../../XbsjEarthUI/assets/demo.mp4';
+            cameraVideo1.position = [1.9017043698837766, 0.5972379094016695, 446.2499351617626];
+            cameraVideo1.rotation = [0.07413323656963833, -0.933639537288121, 0.0003900191769634631];
+            cameraVideo1.far = 50;
+
+            window.cameraVideo1 = cameraVideo1;
+
+            const cameraVideo1SO = new XE.SceneTree.Leaf(cameraVideo1);
+            cameraVideo1SO.title = '视频融合1';
+            g00.children.push(cameraVideo1SO);
+        }
+
+        // 视频融合2
+        {
+            var cameraVideo = new XE.Obj.CameraVideo(xbsjEarth);
+            cameraVideo.videoUrl = '../../../XbsjEarthUI/assets/demo.mp4';
+            cameraVideo.position = [1.9017145371326925, 0.5972610388274915, 447.0411765370373];
+            cameraVideo.rotation = [4.689689783226122, -0.9712893893861931, 6.277658415979939];
+            cameraVideo.far = 50;
+            window.cameraVideo2 = cameraVideo;
+
+            const cameraVideo2SO = new XE.SceneTree.Leaf(cameraVideo);
+            cameraVideo2SO.title = '视频融合2';
+            g00.children.push(cameraVideo2SO);
+        }
+
+        // 视域分析1
+        {
+            var viewshed1 = new XE.Obj.Viewshed(xbsjEarth);
+            viewshed1.position = [1.9016941363233133, 0.5972280994903124, 441.33440879700987];
+            viewshed1.rotation = [5.661008560777628, -0.4002876987466508, 6.281209044159919];
+            viewshed1.far = 50;
+
+            window.viewshed1 = viewshed1;
+
+            const viewshed1SO = new XE.SceneTree.Leaf(viewshed1);
+            viewshed1SO.title = '视域分析1';
+            g00.children.push(viewshed1SO);
+        }
+
+        // 视域分析2
+        {
+            var viewshed2 = new XE.Obj.Viewshed(xbsjEarth);
+            window.viewshed2 = viewshed2;
+            viewshed2.position = [1.9017162550061513, 0.5972198728482121, 433.7983257335933];
+            viewshed2.rotation = [5.48363664103055, -0.32127571902937535, 6.280826152838497];
+            viewshed2.far = 50;
+
+            const viewshed2SO = new XE.SceneTree.Leaf(viewshed2);
+            viewshed2SO.title = '视域分析2';
+            g00.children.push(viewshed2SO);
+        }
+
+        // 多边形1
+        {
+            const positions = [
+                -115.0, 37.0,
+                -115.0, 32.0,
+                -107.0, 33.0,
+                -102.0, 31.0,
+                -102.0, 35.0
+            ].map(e => e / 180 * Math.PI);
+
+            var polygon = new XE.Obj.Polygon(xbsjEarth);
+            polygon.positions = positions;
+            polygon.height = 1000.0;
+            window.polygon = polygon;
+
+            const polygonSO = new XE.SceneTree.Leaf(polygon);
+            polygonSO.title = '多边形1';
+            g00.children.push(polygonSO);
+        }
+
+
+        // 裁剪面1
+        {
+            var clippingPlane = new XE.Obj.ClippingPlane(xbsjEarth);
+            window.clippingPlane = clippingPlane;
+            var t = [108.95921534905067, 34.21957314169267].map(e => e / 180 * Math.PI);
+            clippingPlane.position = [...t, 426.0];
+            const clippingPlaneSO = new XE.SceneTree.Leaf(clippingPlane);
+            clippingPlaneSO.title = '裁剪面1';
+            g00.children.push(clippingPlaneSO);
+
+            // clippingPlane._primitive.tilesets.push(tileset1._tileset);
+        }
+
+        // 压平多边形组
+        {
+            // const positions = [
+            //     -115.0, 37.0,
+            //     -115.0, 32.0,
+            //     -107.0, 33.0,
+            //     -102.0, 31.0,
+            //     -102.0, 35.0
+            // ].map(e => e / 180 * Math.PI);
+            const positions = [
+                108.95921534905067, 34.21957314169267,
+                108.95968034385935, 34.219564234321986,
+                108.95968071326962, 34.21998589644056,
+                108.95920292734705, 34.21998000327241,
+            ].map(e => e / 180 * Math.PI);
+
+            const flattenedPolygon = {
+                positions,
+                height: 426.0,
+            };
+
+            var flattenedPolygons = new XE.Obj.FlattenedPolygonCollection(xbsjEarth);
+            flattenedPolygons.polygons.push(flattenedPolygon);
+            window.flattenedPolygons = flattenedPolygons;
+
+            const flattenedPolygonsSO = new XE.SceneTree.Leaf(flattenedPolygons);
+            flattenedPolygonsSO.title = '压平多边形组1';
+            g00.children.push(flattenedPolygonsSO);
+
+            // 绑定压平多边形
+            tileset1.xbsjFlattenGuid = flattenedPolygons.guid;
+        }
+
+        {
+            const points = new XE.Obj.Points(xbsjEarth);
+            const point = {};
+            point.position = [116.39 * Math.PI / 180.0, 39.9 * Math.PI / 180, 100];
+            point.color = [1, 0, 0, 1];
+            points.points.push(point);
+
+            window.points = points;
+
+            const sceneObject = new XE.SceneTree.Leaf(points);
+            sceneObject.title = '点';
+            g00.children.push(sceneObject);
+        }
+
+        {
+            const polyline = new XE.Obj.Polyline(xbsjEarth);
+            polyline.positions = [[116.39, 39.9], [116.39, 20.9], [100.39, 20.0]].map(p => [...p.map(e => e * Math.PI / 180.0), 0.0]);
+
+            window.polyline = polyline;
+
+            const sceneObject = new XE.SceneTree.Leaf(polyline);
+            sceneObject.title = '折线👇';
+            g00.children.push(sceneObject);
+        }
+
+        {
+            const circle = new XE.Obj.Circle(xbsjEarth);
+            circle.center = [...([116.39, 39.9].map(e => e * Math.PI / 180.0)), 100];
+            circle.radius = 150000;
+
+            window.circle = circle;
+
+            const sceneObject = new XE.SceneTree.Leaf(circle);
+            sceneObject.title = '⚪';
+            g00.children.push(sceneObject);
+        }
+    }
+}
+
+// 2 json格式数据读取
+function sceneTreeCreateExample2() {
+
+    var earthJsonObject = {
+        "sceneTree": {
+            "root": {
+                "expand": true,
+                "title": "未命名",
+                "children": [
+                    {
+                        "expand": true,
+                        "title": "未命名",
+                        "children": [
+                            {
+                                "expand": true,
+                                "title": "未命名",
+                                "children": [
+                                    {
+                                        "czmObject": {
+                                            "type": "XbsjTileset",
+                                            "guid": "c681a2cc-7f08-45a4-a8d3-2a03859b2c35",
+                                            "options": {
+                                                "url": "http://127.0.0.1:81/tileset.json"
+                                            }
+                                        },
+                                        "enabled": true,
+                                        "title": "三维瓦片1"
+                                    },
+                                    {
+                                        "czmObject": {
+                                            "type": "XbsjImagery",
+                                            "guid": "c7c01d0e-322a-4dd6-9195-37bc0e1faa2c",
+                                            "options": {
+                                                "xbsjImageryProvider": {
+                                                    "type": "UrlTemplateImageryProvider",
+                                                    "url": "http://mt1.google.cn/vt/lyrs=s&hl=zh-CN&x={x}&y={y}&z={z}&s=Gali",
+                                                    "rectangle": [
+                                                        -3.141592653589793,
+                                                        -1.5707963267948966,
+                                                        3.141592653589793,
+                                                        1.5707963267948966
+                                                    ]
+                                                }
+                                            }
+                                        },
+                                        "enabled": true,
+                                        "title": "谷歌全球影像"
+                                    },
+                                    {
+                                        "czmObject": {
+                                            "type": "XbsjTerrain",
+                                            "guid": "81534f16-edba-40b1-af66-eefb0392ccfa",
+                                            "options": {
+                                                "xbsjTerrainProvider": {
+                                                    "type": "CesiumTerrainProvider",
+                                                    "url": "http://lab2.cesiumlab.com:9000/terrain/577fd5b0ac1f11e99dbd8fd044883638"
+                                                }
+                                            }
+                                        },
+                                        "enabled": true,
+                                        "title": "谷歌全球地形"
+                                    } 
+                                ]
+                            }
+                        ]
+                    },
+                    {
+                        "expand": true,
+                        "title": "未命名",
+                        "children": []
+                    }
+                ]
+            }
+        }
+    };
+
+    xbsjEarth.fromJson(earthJsonObject);
+}
+
+function sceneTreeCreateExample3(xbsjEarth) {
+
+    const xbsjSceneTree = xbsjEarth.sceneTree;
+    //只加中国地形和谷歌影像
+    const g0 = new XE.SceneTree.Group(xbsjEarth);
+    g0.title = '项目1'
+    xbsjSceneTree.root.children.push(g0);
+
+    // 谷歌影像加载
+    {
+        var googleImageryLayer = new XE.Obj.Imagery(xbsjEarth);
+        window.googleImageryLayer = googleImageryLayer;
+
+        const imageryProvider = new XE.ImageryProvider.XbsjImageryProvider();
+        imageryProvider.url = 'http://mt1.google.cn/vt/lyrs=s&hl=zh-CN&x={x}&y={y}&z={z}&s=Gali';
+        imageryProvider.rectangle = [-Math.PI, -Math.PI * 0.5, Math.PI, Math.PI * 0.5];
+        // googleImageryLayer.xbsjImageryProvider = imageryProvider;
+        googleImageryLayer.xbsjImageryProvider.type = imageryProvider._type;
+        googleImageryLayer.xbsjImageryProvider[imageryProvider._type] = imageryProvider;
+
+        const googleImageryLayerSO = new XE.SceneTree.Leaf(googleImageryLayer);
+        googleImageryLayerSO.title = '谷歌影像';
+        g0.children.push(googleImageryLayerSO);
+    }
+
+    {
+        var chinaTerrain = new XE.Obj.Terrain(xbsjEarth);
+        chinaTerrain.show = true;
+        const xbsjTerrainProvider = new XE.XbsjCesiumTerrainProvider();
+        xbsjTerrainProvider.url = 'http://lab2.cesiumlab.com:9000/terrain/577fd5b0ac1f11e99dbd8fd044883638';
+        xbsjTerrainProvider.requestVertexNormals = true;
+        xbsjTerrainProvider.requestWaterMask = true;
+
+        chinaTerrain.xbsjTerrainProvider = xbsjTerrainProvider;
+
+        window.chinaTerrain = chinaTerrain;
+
+        const chinaTerrainSO = new XE.SceneTree.Leaf(chinaTerrain);
+        chinaTerrainSO.title = '中国地形';
+        g0.children.push(chinaTerrainSO);
+    }
+}
---
+++ Apps/Examples/scripts/codeEditor.js
@@ -0,0 +1,187 @@
+var lastForceCodeTimeStamp = 0;
+
+var delayApply = (function () {
+    var _settingCodeDelay = new rxjs.Subject();
+    var _settingCodeDelaySubscription;
+
+    function delayApply_(code, vueApp) {
+        _settingCodeDelaySubscription = _settingCodeDelaySubscription || _settingCodeDelay.
+            pipe(rxjs.operators.debounceTime(2000)).
+            subscribe(() => {
+                const diffTime = Date.now() - lastForceCodeTimeStamp;
+                if (diffTime > 2000 + 500) {
+                    vueApp.autoRefresh && vueApp.apply();
+                }
+            });
+
+        _settingCodeDelay.next(code);
+    }
+    return delayApply_;
+})();
+
+var setCodeFromUrl = (function () {
+    var _setCodeFromUrlSubscription;
+    function setCodeFromUrl_(url, vueApp) {
+        _setCodeFromUrlSubscription && _setCodeFromUrlSubscription.unsubscribe();
+        _setCodeFromUrlSubscription = undefined;
+
+        _setCodeFromUrlSubscription = _setCodeFromUrlSubscription || rxjs.fetch.fromFetch(url).pipe(
+            rxjs.operators.switchMap(res => {
+                if (res.ok) {
+                    return res.text();
+                } else {
+                    return of({ error: true, message: `Error ${res.status}` });
+                }
+            }),
+            rxjs.operators.catchError(err => {
+                // Network or other error, handle appropriately
+                console.error(err);
+                return of({ error: true, message: err.message })
+            }),
+            rxjs.operators.map(code => {
+                lastForceCodeTimeStamp = Date.now();
+                code && (vueApp.code = code);
+                vueApp.apply();
+            }),
+        ).subscribe(() => {
+            _setCodeFromUrlSubscription && _setCodeFromUrlSubscription.unsubscribe();
+            _setCodeFromUrlSubscription = undefined;
+        });
+    }
+    return setCodeFromUrl_;
+})();
+
+var CodeEditor = {
+    props: {
+        value: String,
+    },
+    template: `
+        <codemirror ref="myCm"
+            :value="value" 
+            :options="cmOptions"
+            @ready="onCmReady"
+            @focus="onCmFocus"
+            @input="onCmCodeChange">
+        </codemirror>
+    `,
+    data() {
+        return {
+            // code: '',
+            cmOptions: {
+                // codemirror options
+                tabSize: 4,
+                // mode: 'text/javascript',
+                mode: 'htmlmixed',
+                // theme: 'base16-dark',
+                // theme: 'base16-light',
+                lineNumbers: true,
+                line: true,
+                // more codemirror options, 更多 codemirror 的高级配置...
+
+                // 以什么格式进行高亮 -->
+                // mode: "text/x-sql",
+                // 主题 -->
+                theme: 'seti',
+                // 是否代码折叠 -->
+                lineWrapping: false,
+             
+                // 是否在编辑器左侧显示行号 -->
+                lineNumbers: true,
+                // 行号从哪个数开始计数，默认为1 -->
+                firstLineNumber: 1,
+             
+                // tab字符的宽度，默认为4 -->
+                indentWithTabs: true,
+                // 自动缩进，设置是否根据上下文自动缩进,默认为true-->
+                smartIndent: true,
+             
+                // 括号匹配 -->
+                matchBrackets : true,
+                // 是否在初始化时自动获取焦点 -->
+                autofocus: true,
+                // 智能提示  -->
+                // extraKeys: {"Ctrl-Space": "autocomplete"},
+                // 编辑器是否只读,并且不能获得焦点 -->
+                // readOnly:'nocursor',
+                // 在选择时是否显示光标，默认为false -->
+                showCursorWhenSelecting:true
+            }
+        }
+    },
+    methods: {
+        onCmReady(cm) {
+        },
+        onCmFocus(cm) {
+        },
+        onCmCodeChange(newCode) {
+            this.$emit('input', newCode);
+        }
+    },
+    computed: {
+        codemirror() {
+            return this.$refs.myCm.codemirror
+        },
+    },
+    mounted() {
+        // console.log('this is current codemirror object', this.codemirror)
+        // you can use this.codemirror to do something...
+    },
+    beforeDestroy() {
+    }
+};
+
+var vueApp = new Vue({
+    components: {
+        CodeEditor,
+    },
+    el: '#vueApp',
+    data: {
+        examples: [],
+        autoRefresh: true,
+        code: '',
+        title: '<未能获取示例名称!>',
+    },
+    mounted() {
+        // console.log('this is current codemirror object', this.codemirror)
+        // you can use this.codemirror to do something...
+    },
+    methods: {
+        setCode(url) {
+            setCodeFromUrl(url, this);
+        },
+        apply() {
+            this.$refs.earthAppFrame.srcdoc = this.code;
+            var titles = /\<title\>([\s\S]*)\<\/title\>/mg.exec(this.code)
+            var title = '<未能获取示例名称!>';
+            if (titles.length >= 2) {
+                title = titles[1];
+            }
+            this.title = title;
+        }
+    },
+    watch: {
+        code(newVal) {
+            delayApply(newVal, this);
+        },
+    }
+});
+
+// earthAppFrame.src方式会改变iframe中文
+// var basePath = location.host + location.pathname;
+// code = code.replace('<head>', `<head><base href="${basePath}" />`);
+
+// var blob = new Blob([code], { type: 'text/html' });
+// var ou = URL.createObjectURL(blob);
+// earthAppFrame.src = ou;
+// URL.revokeObjectURL(ou);
+
+// 只有这种方法才能起作用，但是url会被改掉。
+// earthAppFrame.srcdoc = code;
+
+// var codes = /\<head\>([\s\S]*)\<\/head\>[\s\S]*\<body\>([\s\S]*)\<\/body\>/mg.exec(code)
+// if (codes && codes.length === 3) {
+//     var head = codes[1];
+//     var body = codes[2];
+//     earthAppFrame.contentDocument.head.innerHTML = head;
+//     earthAppFrame.contentDocument.body.innerHTML = body;
+// }
