1828a9bb1db90bdd34285e2705adb44c66660127
vtxf <vtxf@qq.com>
2019-08-08 00:04:20 +0800

    自动生成例子列表

---
+++ Apps/Examples/scripts/list.js
@@ -0,0 +1,101 @@
+vueApp.examples = [
+    {
+        "title": "Beginner",
+        "list": [
+            {
+                "title": "销毁地球",
+                "url": "./earth-destroyEarth.html"
+            }
+        ]
+    },
+    {
+        "title": "Showcases",
+        "list": [
+            {
+                "title": "销毁地球",
+                "url": "./earth-destroyEarth.html"
+            }
+        ]
+    },
+    {
+        "title": "misc",
+        "list": [
+            {
+                "title": "销毁Vue创建的地球",
+                "url": "./earth-destroyEarthWithVue.html"
+            },
+            {
+                "title": "编辑互斥",
+                "url": "./earth-editing.html"
+            },
+            {
+                "title": "3dtiles压平",
+                "url": "./earth-flattenning.html"
+            },
+            {
+                "title": "谷歌影像",
+                "url": "./earth-googleImagery.html"
+            },
+            {
+                "title": "影像数据加载",
+                "url": "./earth-imageries.html"
+            },
+            {
+                "title": "原生Cesium API的调用",
+                "url": "./earth-nativeCesium.html"
+            },
+            {
+                "title": "地下模式和地表透明",
+                "url": "./earth-subsurface.html"
+            },
+            {
+                "title": "创建",
+                "url": "./earthui-basic.html"
+            },
+            {
+                "title": "自定义属性窗口",
+                "url": "./earthui-custom.html"
+            },
+            {
+                "title": "从JSON创建场景结构",
+                "url": "./earthui-fromJSON.html"
+            },
+            {
+                "title": "高德地图纠偏",
+                "url": "./earthui-gaode.html"
+            },
+            {
+                "title": "内置UI组件",
+                "url": "./earthui-xbsjUI.html"
+            },
+            {
+                "title": "示例集合",
+                "url": "./index.html"
+            },
+            {
+                "title": "数据双向绑定-理论模型",
+                "url": "./startup-binds-classic.html"
+            },
+            {
+                "title": "数据双向绑定-理论模型2",
+                "url": "./startup-binds-classic2.html"
+            },
+            {
+                "title": "数据双向绑定",
+                "url": "./startup-binds.html"
+            },
+            {
+                "title": "创建地球",
+                "url": "./startup-createEarth.html"
+            },
+            {
+                "title": "使用Vue创建地球",
+                "url": "./startup-createEarthWithVue.html"
+            },
+            {
+                "title": "3dtiles数据加载",
+                "url": "./startup-tileset.html"
+            }
+        ]
+    }
+]
---
+++ Tools/examples/gallery.js
@@ -0,0 +1,48 @@
+var fs = require('fs');
+var globby = require('globby');
+
+const exampleMap = {};
+
+var fileList = ['Apps/Examples/*.html'];
+globby.sync(fileList).forEach(filePath => {
+    var content = fs.readFileSync(filePath, 'utf-8');
+
+    var results = /\<title\>([\s\S]*)\<\/title\>/mg.exec(content);
+    var title = '<未能获取示例名称!>';
+    if (results && results.length >= 2) {
+        title = results[1];
+    }
+
+    // <meta name="xbsj-labels" content="Beginner, Showcases"></meta>
+    var results2 = /\<meta\sname=\"xbsj-labels\"\scontent=\"(.*)\"\>\<\/meta\>/mg.exec(content);
+    var labels = [];
+    if (results2 && results2.length >= 2) {
+        labels = results2[1].split(',').map(e => e.trim());
+    }
+    if (labels.length === 0) {
+        labels.push('misc');
+    }
+
+    labels.forEach(label => {
+        exampleMap[label] = exampleMap[label] || [];
+        exampleMap[label].push({
+            title: title,
+            url: './' + filePath.slice(14),
+        });
+    });
+});
+
+var exampleList = Object.entries(exampleMap).map(([title, list]) => ({
+    title,
+    list,
+}));
+
+var ss = 'vueApp.examples = ' + JSON.stringify(exampleList, undefined, '    ');
+
+var fd = fs.openSync('Apps/Examples/scripts/list.js', 'w+');
+var buffer = Buffer.from('/* This file is automatically rebuilt by the Cesium build process. */\n');
+
+// fs.writeSync(fd, buffer, 0, buffer.length, 0); //write new data
+fs.writeSync(fd, ss);
+
+fs.close(fd);
