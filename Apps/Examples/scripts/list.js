27483d6e7ce61324fe199c76dbf482c4fac912b0
caiying <2841902102@qq.com>
2019-10-23 16:33:32 +0800

    修改了示例

--- Apps/Examples/scripts/list.js
+++ Apps/Examples/scripts/list.js
@@ -309,6 +309,91 @@ vueApp.examples = [
             {
                 "title": "路径-pin",
                 "url": "./eath-path-pin.html"
+            },
+            {
+                "title": "mapv-热力图",
+                "url": "./earth-mapv-map-point-heatmap.html"
+            },
+            {
+                "title": "mapv-气泡",
+                "url": "./earth-mapv-point-bubble.html"
+            },
+            {
+                "title": "mapv-种类",
+                "url": "./earth-mapv-point-category.html"
+            },
+            {
+                "title": "mapv-集群",
+                "url": "./earth-mapv-point-cluster.html"
+            },
+            {
+                "title": "mapv-点等值线图",
+                "url": "./earth-mapv-point-choropleth.html"
+            },
+            {
+                "title": "mapv-矩形",
+                "url": "./earth-mapv-point-density-rect.html"
+            },
+            {
+                "title": "mapv-蜂窝状",
+                "url": "./earth-mapv-point-honeycomb.html"
+            },
+            {
+                "title": "mapv-点强度",
+                "url": "./earth-mapv-point-intensity.html"
+            },
+            {
+                "title": "mapv-多边形等值线图",
+                "url": "./earth-mapv-polygon-choropleth.html"
+            },
+            {
+                "title": "mapv-多边形强度",
+                "url": "./earth-mapv-polygon-intensity.html"
+            },
+            {
+                "title": "mapv-多段线动画",
+                "url": "./earth-mapv-polyline-animation.html"
+            },
+            {
+                "title": "mapv-简单折线",
+                "url": "./earth-mapv-polyline-simple.html"
+            },
+            {
+                "title": "mapv-webgl",
+                "url": "./earth-mapv-webgl-simple.html"
+            }
+        ]
+    },
+    {
+        "title": "Earth起步",
+        "list": [
+            {
+                "title": "数据双向绑定-理论模型",
+                "url": "./startup-binds-classic.html"
+            },
+            {
+                "title": "数据双向绑定-理论模型2",
+                "url": "./startup-binds-classic2.html"
+            },
+            {
+                "title": "数据双向绑定",
+                "url": "./startup-binds.html"
+            },
+            {
+                "title": "创建地球",
+                "url": "./startup-createEarth.html"
+            },
+            {
+                "title": "使用Vue创建地球",
+                "url": "./startup-createEarthWithVue.html"
+            },
+            {
+                "title": "加载标绘插件",
+                "url": "./startup-curveArrow.html"
+            },
+            {
+                "title": "3dtiles数据加载",
+                "url": "./startup-tileset.html"
             }
         ]
     },
@@ -382,91 +467,6 @@ vueApp.examples = [
             {
                 "title": "内置UI组件",
                 "url": "./earthui-xbsjUI.html"
-            }
-        ]
-    },
-    {
-        "title": "Earth起步",
-        "list": [
-            {
-                "title": "mapv-热力图",
-                "url": "./mapv-map-point-heatmap.html"
-            },
-            {
-                "title": "mapv-气泡",
-                "url": "./mapv-point-bubble.html"
-            },
-            {
-                "title": "mapv-种类",
-                "url": "./mapv-point-category.html"
-            },
-            {
-                "title": "mapv-集群",
-                "url": "./mapv-point-cluster.html"
-            },
-            {
-                "title": "mapv-点色斑",
-                "url": "./mapv-point-colorpleth.html"
-            },
-            {
-                "title": "mapv-矩形",
-                "url": "./mapv-point-density-rect.html"
-            },
-            {
-                "title": "mapv-蜂窝体",
-                "url": "./mapv-point-honeycomb.html"
-            },
-            {
-                "title": "mapv-点强度",
-                "url": "./mapv-point-intensity.html"
-            },
-            {
-                "title": "mapv-多边形色斑",
-                "url": "./mapv-polygon-colorpleth.html"
-            },
-            {
-                "title": "mapv-多边形强度",
-                "url": "./mapv-polygon-intensity.html"
-            },
-            {
-                "title": "mapv-多段线动画",
-                "url": "./mapv-polyline-animation.html"
-            },
-            {
-                "title": "mapv-简单折线",
-                "url": "./mapv-polyline-simple.html"
-            },
-            {
-                "title": "mapv-webgl",
-                "url": "./mapv-webgl-simple.html"
-            },
-            {
-                "title": "数据双向绑定-理论模型",
-                "url": "./startup-binds-classic.html"
-            },
-            {
-                "title": "数据双向绑定-理论模型2",
-                "url": "./startup-binds-classic2.html"
-            },
-            {
-                "title": "数据双向绑定",
-                "url": "./startup-binds.html"
-            },
-            {
-                "title": "创建地球",
-                "url": "./startup-createEarth.html"
-            },
-            {
-                "title": "使用Vue创建地球",
-                "url": "./startup-createEarthWithVue.html"
-            },
-            {
-                "title": "加载标绘插件",
-                "url": "./startup-curveArrow.html"
-            },
-            {
-                "title": "3dtiles数据加载",
-                "url": "./startup-tileset.html"
             }
         ]
     }
