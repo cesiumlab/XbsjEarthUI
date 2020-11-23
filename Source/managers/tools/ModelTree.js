d0b1d80282f59c008cc1d903ddd33b47be435756
wangchengxu <1162251639@qq.com>
2020-11-23 17:51:51 +0800

    ''

--- Source/managers/tools/ModelTree.js
+++ Source/managers/tools/ModelTree.js
@@ -6,6 +6,7 @@ class ModelTree {
     constructor(root) {
         this._root = root;
         this._comp = root._comp.$refs.modelTreeTool[0];
+        console.log( root);
 
         XE.MVVM.extend(this, {
 /**
